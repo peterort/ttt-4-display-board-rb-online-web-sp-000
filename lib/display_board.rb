fe3985a014fabf51197d3df19c0716cb42b863f6
Peter Ort <peter@peterort.com>
2020-02-05 18:25:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+display_board(board)
+
+display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
