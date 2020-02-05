2af4c8c0c457e1246377a31508303b3119b45d83
Peter Ort <peter@peterort.com>
2020-02-05 18:32:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ blank_board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board = blank_board)
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
-  puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
+  puts " #{board[3]} | #{board[4]}  | #{board[5]}  "
   puts "-----------"
   puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
